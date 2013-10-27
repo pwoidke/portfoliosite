0af8ffca82293080f1772139be2883535c9f7c7a
Paul W. <pwoidke@gmail.com>
2013-10-26 20:04:11 -0400

    Got nav button animations working

--- New/js/main.js
+++ New/js/main.js
@@ -1,32 +1,55 @@
 var degrees = -15;
-
 $(document).ready(function(){
 
     $('.parallelogram').hover(
         function(){
-            $(this).stop().animate({
-                skewX: '0deg'
-            });
-            $(this).children().stop().animate({
-                skewX: '0deg'
-            });
-            $(this).addClass('square');
+            if(!$(this).hasClass('full'))
+            {
+                $(this).stop().animate({
+                    skewX: '0deg'
+                });
+                $(this).children().stop().animate({
+                    skewX: '0deg'
+                });
+                $(this).addClass('square');
+            }
         },
         function(){
-            $(this).stop().animate({
-                skewX: degrees + 'deg'
-            });
-            $(this).children().stop().animate({
-                skewX: -degrees + 'deg'
-            });
-            $(this).removeClass('square');
+            if(!$(this).hasClass('full'))
+            {
+                $(this).stop().animate({
+                    skewX: degrees + 'deg'
+                });
+                $(this).children().stop().animate({
+                    skewX: -degrees + 'deg'
+                });
+                $(this).removeClass('square');
+            }
         }
     );
 
     $('.parallelogram').click(function(){
-        $(this).animate({
-//            height: 500
-        });
+        if($(this).hasClass('full'))
+        {
+            $(this).animate({
+                skewX: degrees + 'deg',
+                width: '13em'
+            }, 200, function(){ $(this).removeClass('full'); });
+            $(this).children().animate({
+                skewX: -degrees + 'deg'
+            });
+        }
+        else
+        {
+            $(this).animate({
+                skewX: '0deg',
+                width: '100%'
+            });
+            $(this).children().animate({
+                skewX: '0deg'
+            });
+            $(this).addClass('full');
+        }
     });
 
     $('.parallelogram').css(
