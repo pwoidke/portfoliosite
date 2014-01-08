d14a6867bb85891661229a20e6fbd2eedc2d82d1
Paul W. <pwoidke@gmail.com>
2014-01-08 14:42:35 -0500

    Updated graph padding to fix iOS display

--- New/js/main.js
+++ New/js/main.js
@@ -6,7 +6,7 @@ else { var click = 'click'; }
 
 $(document).ready(function(){
 
-    $('#skillList').find('li').width( Math.floor(100 / $('#skillList').find('li').length) -.7 + "%" );
+    $('#skillList').find('li').width( Math.floor(100 / $('#skillList').find('li').length) - 1 + "%" );
 
     $('#expPlot').scatter({ height: 300, width: '100%', xLabel: 'Proficiency', yLabel: 'Enjoyment', rows: 5, columns: 5, subsections: 4, responsive: true });
     $('#artProgramsGraph').scatter({ height: 300, width: '100%', xLabel: 'Program', yLabel: 'Experience', rows: 5, columns: 5, subsections: 4, responsive: true });
