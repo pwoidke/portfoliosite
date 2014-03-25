5a224662edbd655f49a4c17f475404c0c2122a40
Paul W. <pwoidke@gmail.com>
2014-03-25 15:33:39 -0400

    Fixed toggle update, may have broken other stuff
    Added image-lightbox jQuery plugin for future use with gallery (once I
    figure out how to initialize it on a hidden element. I may have to
    change the site to initialize with everything open, initialize stuff,
    and then close everything. We'll see.)

--- New/js/main.js
+++ New/js/main.js
@@ -4,15 +4,33 @@ var degrees = -15;
 var click = 'click';
 if( 'ontouchstart' in window ){ click = 'touchstart'; }
 
+function checkOpen() {
+    var full = $('.full'),
+        toggleButton = $('#toggleButton');;
+    if (full.length === $('.parallelogram').length)
+    {
+        toggleButton.html("&minus;");
+    }
+
+    if (full.length === 0)
+    {
+        toggleButton.html("+");
+    }
+}
+
 $(document).ready(function(){
 
-    var parallelograms = $('.parallelogram');
-    var skillList = $('#skillList');
+    var parallelograms = $('.parallelogram'),
+        skillList = $('#skillList'),
+        toggleButton = $('#toggleButton');
+
+    // Equalize spacing between skill bars in graph
     skillList.find('li').width( Math.floor(100 / skillList.find('li').length) - 1 + "%" );
 
     $('#expPlot').scatter({ height: 300, width: '100%', xLabel: 'Proficiency', yLabel: 'Passion', rows: 5, columns: 5, subsections: 4, responsive: true });
     $('#artProgramsGraph').scatter({ height: 300, width: '100%', xLabel: 'Program', yLabel: 'Experience', rows: 5, columns: 5, subsections: 4, responsive: true });
 
+    // Set language background colors
     $('.projectTech li').each(function () {
         if($(this).html() === "C#") {
             $(this).css('background-color', '#5A25A2');
@@ -68,7 +86,10 @@ $(document).ready(function(){
             $(this).animate({
                 skewX: degrees + 'deg',
                 width: '65%'
-            }, 200, function(){ $(this).removeClass('full'); });
+            }, 200, function(){
+                $(this).removeClass('full');
+                checkOpen();
+            });
             $(this).children().animate({
                 skewX: -degrees + 'deg'
             });
@@ -78,50 +99,57 @@ $(document).ready(function(){
             $(this).animate({
                 skewX: '0deg',
                 width: '100%'
+            }, 200, function(){
+                $(this).addClass('full');
+                checkOpen();
             });
             $(this).children().animate({
                 skewX: '0deg'
             });
             $(this).find('article').show(200);
-            $(this).addClass('full');
 
             // Check and trigger section-specific events here
             var i = 0;
             switch ($(this).children('h3').html()) {
                 case "About":
-                    break;
+
+
+
+                break;
                 case "Code":
+
+                    // Show points
                     var points = $('#expPlot').find('.point');
                     window.setTimeout(function(){
                         (function displayPoints() {
                             points.eq(i++).fadeIn(300, displayPoints);
                         })();
                     }, 600);
-                    break;
+
+                break;
                 case "Art &amp; Design":
+
+
+
                 break;
                 case "Projects":
-                    break;
+
+
+
+                break;
                 case "Contact":
+
+                    // Show icons
                     var links = $('#socialLinks').find('a img');
                     window.setTimeout(function(){
-                        (function displayImages() {  
-                            links.eq(i++).fadeIn(300, displayImages);  
+                        (function displayImages() {
+                            links.eq(i++).fadeIn(300, displayImages);
                         })();
                     }, 600);
+
                 break;
             }
         }
-
-//        if ($('.full').length === $('.parallelogram').length)
-//        {
-//            $('.toggleButton').html("&minus;");
-//        }
-//
-//        if ($(".full").length == 0)
-//        {
-//            $('.toggleButton').html("+");
-//        }
     });
     
     $('article').click(function (e) { e.stopPropagation(); });
@@ -138,19 +166,26 @@ $(document).ready(function(){
         '-moz-transform': 'skewX(' + -degrees + 'deg)',
         '-webkit-transform': 'skewX(' + -degrees + 'deg)'
     });
-    
-    $('.toggleButton').click(function () {
+
+    toggleButton.click(function () {
         if($(this).html() === "+")
         {
+            parallelograms.each(function(){
+                if(!$(this).hasClass('full')){
+                    $(this).click();
+                }
+            });
             $(this).html("&minus;");
-            $('.parallelogram').removeClass('full');
         }
         else
         {
+            parallelograms.each(function(){
+                if($(this).hasClass('full')){
+                    $(this).click();
+                }
+            });
             $(this).html("+");
-            $('.parallelogram').addClass('full');
         }
-        parallelograms.click();
     });
     
     $('.projectResponsibilitiesHeader').click(function () {
@@ -159,26 +194,15 @@ $(document).ready(function(){
         if($(this).next().is(':visible'))
         {
             $(this).next().hide(200);
+            toggle.html("+");
+            $(this).next().addClass('responsibilitiesShow');
         }
         else
         {
             $(this).next().show(200);
-        }
-
-        if(toggle.html() === "+")
-        {
             toggle.html("&minus;");
             $(this).next().removeClass('responsibilitiesShow');
         }
-        else
-        {
-            toggle.html("+");
-            $(this).next().addClass('responsibilitiesShow');
-        }
     });
-
-
-    /* GALLERY */
-
 
 });
